ad4a43149a5910262279812ae625d8a67de21b94
heriveltogabriel <heriveltogabriel@gmail.com>
2024-11-19 18:11:20 -0300

    Mudar variavel do repro

--- repo-container.tf
+++ repo-container.tf
@@ -1,5 +1,5 @@
 resource "oci_artifacts_container_repository" "container_repository" {
     #Required
     compartment_id = var.compartment_id
-    display_name_2 = var.container_repository_display_name_back
+    display_name = var.container_repository_display_name_back
 }
