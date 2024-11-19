39c1f0ae90781713ea674ad82451fa73ef0eff73
heriveltogabriel <heriveltogabriel@gmail.com>
2024-11-19 16:04:43 -0300

    Mudança Repo Back

--- repo-container.tf
+++ repo-container.tf
@@ -1,10 +1,10 @@
-resource "oci_artifacts_container_repository" "container_repository" {
+resource "oci_artifacts_container_repository_front" "container_repository" {
     #Required
     compartment_id = var.compartment_id
     display_name = var.container_repository_display_name_front
 }
 
-resource "oci_artifacts_container_repository" "container_repository" {
+resource "oci_artifacts_container_repository_back" "container_repository" {
     #Required
     compartment_id = var.compartment_id
     display_name = var.container_repository_display_name_back
